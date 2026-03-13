596a412ee69fa78ed2c368c827552303d6ed512d
unknown <manimbomia=ka@gmail.com>
2026-03-13 16:44:38 +0800

    lab-act-instance

---
+++ manimbo-client/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App!</h1>
+        <p>
+          Name: Mika Angela Manimbo<br />
+          Email: manimbomika@gmail.com <br />
+          Birthday: July 1, 2002 <br />
+          Place of Birth: Manila
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
